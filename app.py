0a1a6892d938c2d3ea114670d71cdbc25991a86f
iandioch <iandioch11@gmail.com>
2017-02-10 15:03:54 +0000

    Add template rendering

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from jinja2 import Template, Environment, FileSystemLoader
+
+TEMPLATE_DIR = '/home/soso/prog/coireacht/templates'
+
 app = Flask(__name__)
+env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+
+def render_template(name, d):
+    # d should be a dict of key:values to populate the template
+    template = env.get_template(name)
+    return template.render(d)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html', {})
+
+@app.route("/details/<eircode>/")
+def details(eircode):
+    return render_template('details.html', {})
 
 if __name__ == "__main__":
     app.run()
