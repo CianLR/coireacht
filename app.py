4082fe2bc3bcc2ffbd748116db051fdda6ac3d6b
CianLR <cian.ruane1@gmail.com>
2017-02-10 15:14:21 +0000

    Stuff

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import urllib
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,5 +8,14 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/test/<eircode>")
+def eir_to_cord(eircode):
+    url = "https://hackday.autoaddress.ie/2.0/FindAddress?key={}&address={}"
+    key = 'GovHackYourWay-AATmpKey-630E84BE0C4B'
+    final = url.format(key, eircode.replace(' ', '%20'))
+    print(final)
+    resp = urllib.request.urlopen(final)
+    return resp#json.load(resp)
+        
 if __name__ == "__main__":
     app.run()
