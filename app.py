cc24d9a8f3f4928f0613fb3a84c48622bd69fb95
iandioch <iandioch11@gmail.com>
2017-02-10 17:35:13 +0000

    Add star rating

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from jinja2 import Template, Environment, FileSystemLoader
 
 from garda_stations import Station
 
-TEMPLATE_DIR = '/home/waterloo/prog/coireacht/templates'
+TEMPLATE_DIR = '/home/soso/prog/coireacht/templates'
 
 app = Flask(__name__)
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
@@ -67,7 +67,7 @@ def details():
     eircode = request.args.get('eircode')
     addr_data = eir_to_cord(eircode)
     coords = addr_data[1]
-    crime_score = score_for_eircode(coords)
+    crime_score = score_for_coords(coords)
     #coords = addr_data[1].strip('()').split(',') # is a string, not a tuple
     d = {
         'eircode': eircode,
@@ -75,7 +75,7 @@ def details():
         'coord_x': coords[0], 
         'coord_y': coords[1],
         'true_score': crime_score,
-        'rounded_score': crime_score,
+        'rounded_score': round(crime_score),
     }
     print(d)
     return render_template('details.html', d)
